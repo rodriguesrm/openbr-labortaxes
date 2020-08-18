c350448e646f02660b8f93e658811626511541ee
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-08-17 21:23:02 -0300

    Added irpf documents/entities

---
+++ src/OpenBr.LaborTaxes.Business/Documents/IrpfTaxRange.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenBr.LaborTaxes.Business.Documents
+{
+
+    /// <summary>
+    /// Contains details of the tax range
+    /// </summary>
+    public class IrpfTaxRange
+    {
+
+        /// <summary>
+        /// Contains details of the contribution range
+        /// </summary>
+        public class IncomeTaxRange
+        {
+
+            /// <summary>
+            /// Range start value (revenue)
+            /// </summary>
+            public decimal StartValue { get; set; }
+
+            /// <summary>
+            /// Range end value (revenue)
+            /// </summary>
+            public decimal EndValue { get; set; }
+
+            /// <summary>
+            /// Contribution rate (percentage to be applied)
+            /// </summary>
+            public decimal Rate { get; set; }
+
+            /// <summary>
+            /// Amount to be deducted (for progressive taxes)
+            /// </summary>
+            public decimal DeductionAmount { get; set; }
+
+        }
+
+    }
+
+}
